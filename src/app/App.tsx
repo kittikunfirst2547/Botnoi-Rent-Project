685bebbe1655e4d7b7ccf9256fef49fb2e90da12
dev_Hugo <thanandorn.sris@bumail.net>
2026-04-23 21:18:07 +0700

    เพิ่มเคน

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -150,7 +150,7 @@ export default function App() {
                     href="#"
                     className="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-black dark:hover:text-white transition-colors"
                   >
-                    เกี่ยวกับเรา
+                    เกี่ยวกับเคน
                   </a>
                   <a
                     href="#"
